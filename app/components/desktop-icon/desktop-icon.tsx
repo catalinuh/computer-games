20912a333bb18e48358206e4eecb8fde8736a597
Catalina <catalinamcquade@gmail.com>
2026-01-19 06:20:01 -0500

    tried implementing bounds for draggable components

--- app/components/desktop-icon/desktop-icon.tsx
+++ app/components/desktop-icon/desktop-icon.tsx
@@ -1,6 +1,8 @@
+'use client'
 import { Dispatch, ReactNode, SetStateAction, useRef } from 'react'
 import Draggable from 'react-draggable'
 
+import useWindowSize from '../../../app/hooks/useWindowSize'
 import './desktop-icon.scss'
 
 interface DesktopIconProps {
@@ -15,16 +17,26 @@ export default function DesktopIcon({
   name,
 }: DesktopIconProps) {
   const nodeRef = useRef(null)
+  const { width, height } = useWindowSize()
 
   const handleClick = () => {
     setOpenWindow('calculator')
   }
 
   return (
-    <Draggable nodeRef={nodeRef}>
+    <Draggable
+      nodeRef={nodeRef}
+      bounds={{
+        top: 0,
+        // TODO: Fix these 2 measurements
+        right: width ? width - 20 : undefined,
+        bottom: height ? height - 20 : undefined,
+        left: 0,
+      }}
+    >
       <div className="desktop-icon" onDoubleClick={handleClick} ref={nodeRef}>
         {icon}
-        {name}
+        <div className="desktop-icon__name">{name}</div>
       </div>
     </Draggable>
   )
