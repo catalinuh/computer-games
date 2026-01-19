3684eb63ee8abcbe62e62bdf7fd9eda63d77de1f
Catalina <catalinamcquade@gmail.com>
2026-01-19 00:36:14 -0500

    fixed mui hydration error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
-import './styles/globals.css'
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
+import './styles/globals.scss'
 
 export const metadata: Metadata = {
   title: 'Computer Games',
@@ -24,10 +15,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className="antialiased">
+        <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
       </body>
     </html>
   )
