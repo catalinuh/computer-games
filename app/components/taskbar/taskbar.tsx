be9eed9923246ce89485547e00974e9a667fa92d
Catalina <catalinamcquade@gmail.com>
2026-01-31 00:42:56 -0500

    started handling click of start button

--- app/components/taskbar/taskbar.tsx
+++ app/components/taskbar/taskbar.tsx
@@ -1,13 +1,19 @@
-import WindowIcon from '@mui/icons-material/Window'
+import { useState } from 'react'
 
 import Button from '../button/button'
 import './taskbar.scss'
 
 export default function Taskbar() {
+  const [menuIsOpen, setMenuIsOpen] = useState(false)
+
+  const handleClick = () => {
+    setMenuIsOpen((prevIsOpen) => !prevIsOpen)
+  }
+
   return (
     <div className="taskbar">
-      <Button>
-        <span>𝓒𝓜 Catalina McQuade</span>
+      <Button onClick={handleClick}>
+        <span className="taskbar__start-btn-label">𝓒𝓜 Catalina McQuade</span>
       </Button>
     </div>
   )
