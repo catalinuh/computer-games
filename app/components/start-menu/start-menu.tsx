3572e9323a73077815bf846bb608c289de7b8f34
Catalina <catalinamcquade@gmail.com>
2026-01-30 21:01:00 -0500

    created new component StartMenu

---
+++ app/components/start-menu/start-menu.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react'
+
+import './start-menu.scss'
+
+interface StartMenuProps {
+  children?: ReactNode
+}
+
+export default function StartMenu({ children }: StartMenuProps) {
+  return <div className="start-menu">{children}</div>
+}
