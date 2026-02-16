5a7e8cda8378c5e608e988ae80a10f4febdaaf33
Catalina <catalinamcquade@gmail.com>
2026-02-15 20:04:48 -0500

    deleted Button component from app/components/index.ts

--- app/components/index.tsx
+++ app/components/index.tsx
@@ -1,7 +1,6 @@
-import Button from './button/button'
 import DesktopIcon from './desktop-icon/desktop-icon'
 import StartMenu from './start-menu/start-menu'
 import Taskbar from './taskbar/taskbar'
 import Window from './window/window'
 
-export { Button, DesktopIcon, StartMenu, Taskbar, Window }
+export { DesktopIcon, StartMenu, Taskbar, Window }
