24a646021b451c5bf03835dfe52ab52372ae89e8
Catalina <catalinamcquade@gmail.com>
2026-02-25 02:30:56 -0500

    fixed positioning of start menu

--- app/components/taskbar/taskbar.tsx
+++ app/components/taskbar/taskbar.tsx
@@ -45,33 +45,35 @@ export default function Taskbar({
   }
 
   return (
-    <div className="taskbar">
+    <div className="taskbar__container">
       {menuIsOpen ? <StartMenu /> : null}
-      <div className="taskbar__start-btn-and-windows">
-        <button
-          onClick={handleToggleStartMenu}
-          className={`taskbar__start-btn ${className}`}
-        >
-          𝓒𝓜 Catalina McQuade
-        </button>
-        <div className="taskbar__divider"></div>
-        {openWindows.map((window: WindowType, index: number) => (
+      <div className="taskbar">
+        <div className="taskbar__start-btn-and-windows">
           <button
-            key={window}
-            onClick={() => handleToggleWindow(window)}
-            className={`taskbar__window-btn ${activeWindow === window ? 'active' : ''}`}
+            onClick={handleToggleStartMenu}
+            className={`taskbar__start-btn ${className}`}
           >
-            <span>{icons[index]}</span>
-            <span
-              className={`taskbar__window-btn--label ${activeWindow === window ? 'active' : ''}`}
-            >
-              {window}
-            </span>
+            𝓒𝓜 Catalina McQuade
           </button>
-        ))}
-      </div>
-      <div className="taskbar__time">
-        <span>{currentTime.format('hh:mm A')}</span>
+          <div className="taskbar__divider"></div>
+          {openWindows.map((window: WindowType, index: number) => (
+            <button
+              key={window}
+              onClick={() => handleToggleWindow(window)}
+              className={`taskbar__window-btn ${activeWindow === window ? 'active' : ''}`}
+            >
+              <span>{icons[index]}</span>
+              <span
+                className={`taskbar__window-btn--label ${activeWindow === window ? 'active' : ''}`}
+              >
+                {window}
+              </span>
+            </button>
+          ))}
+        </div>
+        <div className="taskbar__time">
+          <span>{currentTime.format('hh:mm A')}</span>
+        </div>
       </div>
     </div>
   )
