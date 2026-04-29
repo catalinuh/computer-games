0d87b592931baa8d9e4b884ac8d6bdabe85f481e
Catalina <catalinamcquade@gmail.com>
2026-04-29 18:28:17 -0400

    Added menu bar for text files

--- app/components/window/window.tsx
+++ app/components/window/window.tsx
@@ -3,7 +3,6 @@ import Draggable from 'react-draggable'
 
 import { WindowType } from '../../page'
 import './window.scss'
-import '98.css'
 
 interface WindowProps {
   activeWindow: WindowType
@@ -76,6 +75,35 @@ export default function Window({
             <button aria-label="Close" onClick={handleClose} />
           </div>
         </div>
+
+        {className === 'text-file' ? (
+          <div className="menus" role="menubar" style={{ touchAction: 'none' }}>
+            <div className="menu-button file-menu-button" role="menuitem">
+              <span>
+                <span className="menu-hotkey">F</span>ile
+              </span>
+            </div>
+            <div className="menu-button edit-menu-button" role="menuitem">
+              <span>
+                <span className="menu-hotkey">E</span>dit
+              </span>
+            </div>
+            <div className="menu-button search-menu-button" role="menuitem">
+              <span>
+                <span className="menu-hotkey">S</span>earch
+              </span>
+            </div>
+            <div className="menu-button help-menu-button" role="menuitem">
+              <span>
+                <span className="menu-hotkey">H</span>elp
+              </span>
+            </div>
+          </div>
+        ) : (
+          <></>
+        )}
+
+        {/* TODO: See if i need className here */}
         <div className={`window-body ${className}`}>{children}</div>
       </div>
     </Draggable>
