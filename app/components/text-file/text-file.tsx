841c1ce6a3f9eb6d3ceef82a308f5c66e0ea0804
Catalina <catalinamcquade@gmail.com>
2026-04-29 18:27:38 -0400

    Moved 98.css import to layout.tsx

--- app/components/text-file/text-file.tsx
+++ app/components/text-file/text-file.tsx
@@ -9,6 +9,7 @@ interface TextFileProps {
 export default function TextFile({ children }: TextFileProps) {
   const divRef = useRef<HTMLDivElement>(null)
 
+  // TODO: Remember what this does and leave a comment about it. I think it focuses the text file when it's opened so that you can immediately start typing??????
   useEffect(() => {
     if (divRef.current) {
       divRef.current.focus()
