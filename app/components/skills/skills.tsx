d21d293f1561d51ab66dadac5a73b68ecf759834
Catalina <catalinamcquade@gmail.com>
2026-05-11 23:25:17 -0400

    moved skills to object array to be iterated through to display

--- app/components/skills/skills.tsx
+++ app/components/skills/skills.tsx
@@ -1,3 +1,5 @@
+import { skills } from '../../pages'
+
 import './skills.scss'
 
 export default function Skills() {
@@ -6,93 +8,43 @@ export default function Skills() {
       <ul className="tree-view">
         <li>Skills</li>
         <li>
-          <details open>
-            <summary>JavaScript</summary>
-            <ul>
-              <li>
-                <details open>
-                  <summary>Frontend</summary>
-                  <ul>
-                    <li>React</li>
-                    <li>React Native</li>
-                    <li>Next.js</li>
-                    <li>TypeScript</li>
-                    <li>Day.js</li>
-                    <li>Storybook</li>
-                    <li>Redux</li>
-                    <li>Recoil</li>
-                  </ul>
-                </details>
-              </li>
+          {skills.map((skill, index) => {
+            const skillGroup = Object.keys(skill)[0]
+            const skillSubCategory = skill[skillGroup]
 
-              <li>
-                <details open>
-                  <summary>Backend</summary>
-                  <ul>
-                    <li>Node.js</li>
-                    <li>Express.js</li>
-                    <li>MongoDB</li>
-                    <li>GQL</li>
-                    <li>Prisma</li>
-                    <li>NestJS</li>
-                    <li>Liquibase</li>
-                    <li>PostgreSQL</li>
-                    <li>SQL</li>
-                    <li>Webpack</li>
-                    <li>Babel.js</li>
-                  </ul>
-                </details>
-              </li>
+            return (
+              <details key={index} open>
+                <summary>{skillGroup}</summary>
 
-              <li>
-                <details open>
-                  <summary>Testing</summary>
+                {typeof skillSubCategory === 'object' &&
+                !Array.isArray(skillSubCategory) ? (
                   <ul>
-                    <li>React Testing Library</li>
-                    <li>Jest</li>
-                    <li>Cypress</li>
-                    <li>Jasmine</li>
-                    <li>Mocha</li>
-                    <li>Chai</li>
-                  </ul>
-                </details>
-              </li>
+                    {Object.entries(skillSubCategory).map(
+                      ([category, skills], idx) => (
+                        <li key={idx}>
+                          <details key={idx} open>
+                            <summary>{category}</summary>
 
-              <li>
-                <details open>
-                  <summary>Misc.</summary>
+                            <ul>
+                              {skills.map((skill, i) => (
+                                <li key={i}>{skill}</li>
+                              ))}
+                            </ul>
+                          </details>
+                        </li>
+                      )
+                    )}
+                  </ul>
+                ) : (
                   <ul>
-                    <li>Socket.io</li>
-                    <li>Phaser 4</li>
-                    <li>Lodash</li>
+                    {skillSubCategory.map((skill, i) => (
+                      <li key={i}>{skill}</li>
+                    ))}
                   </ul>
-                </details>
-              </li>
-            </ul>
-          </details>
-        </li>
-
-        <li>
-          <details>
-            <summary>Other</summary>
-            <ul>
-              <li>HTML/HTML5</li>
-              <li>CSS/SCSS</li>
-              <li>Python</li>
-              <li>Java</li>
-              <li>C#</li>
-              <li>C++</li>
-              <li>Canva</li>
-              <li>Figma</li>
-              <li>Git</li>
-              <li>GitHub</li>
-              <li>GitLab</li>
-              <li>VS Code</li>
-              <li>NetBeans</li>
-              <li>Adobe Photoshop</li>
-              <li>Adobe Illustrator</li>
-            </ul>
-          </details>
+                )}
+              </details>
+            )
+          })}
         </li>
       </ul>
     </div>
