195e67fb430ca4d51bbffcd8cce51f94664ae0bb
Catalina <catalinamcquade@gmail.com>
2026-05-11 23:27:16 -0400

    created projects object and fixed about me info

---
+++ app/pages/index.ts
@@ -0,0 +1,7 @@
+import { aboutMe } from './about-me'
+import { experienceMap, experienceType } from './experience'
+import { projects } from './projects'
+import { skills } from './skills'
+
+export { aboutMe, experienceMap, projects, skills }
+export type { experienceType }
