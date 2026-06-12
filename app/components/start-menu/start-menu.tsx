b30d2021e3d5db2df5f5b8d969e87c812b820d43
Catalina <catalinamcquade@gmail.com>
2026-06-11 22:13:00 -0400

    created start menu banner and styled it

--- app/components/start-menu/start-menu.tsx
+++ app/components/start-menu/start-menu.tsx
@@ -10,7 +10,12 @@ export default function StartMenu({}: StartMenuProps) {
   return (
     // window and window-body classes are for 98.css
     <div className="window start-menu">
-      <div className="window-body">Start menu coming soon!</div>
+      <div className="window-body start-menu__body">
+        <div className="start-menu__banner">Catalina McQuade</div>
+        <div className="start-menu__programs">
+          <div className="start-menu__programs--program">About Me</div>
+        </div>
+      </div>
     </div>
   )
 }
